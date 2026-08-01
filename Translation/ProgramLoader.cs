782c050f5c00449f4bc390d696a3c57d6e32f65b
Erik <54033728+ErikGXDev@users.noreply.github.com>
2026-08-01 12:50:53 +0200

    Error handling

--- Translation/ProgramLoader.cs
+++ Translation/ProgramLoader.cs
@@ -7,7 +7,6 @@ namespace UnbeatableTranslations.Translation
 {
     public class ProgramLoader
     {
-
         public static bool disableCustomTranslation = false;
 
         public static string GetLocalTranslationDirectory()
@@ -28,69 +27,67 @@ namespace UnbeatableTranslations.Translation
 
         public static void LoadLocalTranslations()
         {
-
-            ProgramIndex.programs.Clear();
-            ProgramIndex.lines.Clear();
-
-            // Get the directory of the custom translations
-            string translationDir = GetLocalTranslationDirectory();
-
-            // Get all files in the directory
-            // *.yarnproject.json and lines.json
-            string[] files = Directory.GetFiles(translationDir, "*.yarnproject.json", SearchOption.AllDirectories);
-            foreach (string file in files)
+            try
             {
-                string programText = File.ReadAllText(file);
+                ProgramIndex.programs.Clear();
+                ProgramIndex.lines.Clear();
 
-                Yarn.Program program = Yarn.Program.Parser.ParseJson(programText);
+                // Get the directory of the custom translations
+                string translationDir = GetLocalTranslationDirectory();
 
-                string name = Path.GetFileNameWithoutExtension(file);
-
-                if (name.LastIndexOf('.') != -1)
+                // Get all files in the directory
+                // *.yarnproject.json and lines.json
+                string[] files = Directory.GetFiles(translationDir, "*.yarnproject.json", SearchOption.AllDirectories);
+                foreach (string file in files)
                 {
-                    name = name.Substring(0, name.LastIndexOf('.'));
+                    string programText = File.ReadAllText(file);
+
+                    Yarn.Program program = Yarn.Program.Parser.ParseJson(programText);
+
+                    string name = Path.GetFileNameWithoutExtension(file);
+
+                    if (name.LastIndexOf('.') != -1)
+                    {
+                        name = name.Substring(0, name.LastIndexOf('.'));
+                    }
+
+
+                    Plugin.Logger.LogInfo("Loaded program: " + name);
+
+                    ProgramIndex.programs.Add(name, program);
                 }
 
-
-                Plugin.Logger.LogInfo("Loaded program: " + name);
-
-                ProgramIndex.programs.Add(name, program);
-            }
-
-            files = Directory.GetFiles(translationDir, "lines.json", SearchOption.AllDirectories);
-            foreach (string file in files)
-            {
-                string jsonText = File.ReadAllText(file);
-
-                Dictionary<string, string> lines = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
-                foreach (KeyValuePair<string, string> line in lines)
+                files = Directory.GetFiles(translationDir, "lines.json", SearchOption.AllDirectories);
+                foreach (string file in files)
                 {
-                    ProgramIndex.lines.Add(line.Key, line.Value);
+                    string jsonText = File.ReadAllText(file);
+
+                    Dictionary<string, string> lines =
+                        JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
+                    foreach (KeyValuePair<string, string> line in lines)
+                    {
+                        ProgramIndex.lines.Add(line.Key, line.Value);
+                    }
+                }
+
+                Plugin.Logger.LogInfo("Loaded lines: " + ProgramIndex.lines.Count);
+
+
+                files = Directory.GetFiles(translationDir, "unity_translations.json", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    string jsonText = File.ReadAllText(file);
+
+                    UnityTable table = UnityTable.FromJson(jsonText);
+
+                    ProgramIndex.unityTable = table;
                 }
             }
-
-            Plugin.Logger.LogInfo("Loaded lines: " + ProgramIndex.lines.Count);
-            
-            
-            files = Directory.GetFiles(translationDir, "unity_translations.json", SearchOption.AllDirectories);
-            foreach (string file in files)
+            catch (System.Exception e)
             {
-                string jsonText = File.ReadAllText(file);
-
-                UnityTable table = UnityTable.FromJson(jsonText);
-                
-                ProgramIndex.unityTable = table;
-                
-
+                Plugin.Logger.LogError("Error loading local translations: " + e.Message);
+                Plugin.Logger.LogError(e.StackTrace);
             }
-            
-            Plugin.Logger.LogInfo("Loaded unity table translations (json size): " + ProgramIndex.unityTable.ToJson().Length);
-
         }
-
-
-       
     }
-
-
 }
